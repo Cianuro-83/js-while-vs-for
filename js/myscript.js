d22c6b9bff3e76df634cd05107b704c143823f8f
Cianuro-83 <buzzeosimone@yahoo.com>
2023-02-07 18:59:38 +0100

    FIX uscita dal ciclo, stampa & FINISH

--- js/myscript.js
+++ js/myscript.js
@@ -4,15 +4,33 @@ console.log("CIAO CIANURO: oggi CICLO WHILE vs CICLO FOR");
 let numero;
 // creo array dove pushare i numeri dispari
 let numeriDispari = [];
+let totale = numeriDispari.length;
 // chiedo numero all'utente
-// do {
-numero = parseInt(prompt("Inserisci un numero: "));
-console.log(numero);
-while (isNaN(numero) || numero <= 0 || numeriDispari.lenght >= 10);
-{
+// while (isNaN) {
+//   numero = parseInt(prompt("Inserisci il numero: "));
+//   console.log(numero);
+
+//   for (let i = 0; i < 9; i++) {
+//     if (numero % 2 === 1) {
+//       numeriDispari.push(numero);
+//       console.log("hai i seguenti numeri dispari " + numeriDispari);
+//     }
+//     totale++;
+//   }
+// }
+
+while (numeriDispari.length < 10) {
+  numero = parseInt(prompt("Inserisci il numero: "));
+  console.log(numero);
   if (numero % 2 === 1) {
     numeriDispari.push(numero);
   }
   // chiusura ciclo while
 }
-console.log("hai inserito " + numero, numeriDispari);
+console.log("il tuo array ha " + numeriDispari.length + " numeri");
+console.log("hai i seguenti numeri dispari " + numeriDispari);
+document.write(
+  "il tuo array è composto da " + numeriDispari.length + " numeri " + "<br>"
+);
+for (let i = 0; i < numeriDispari.length; i++)
+  document.write("<br>" + numeriDispari[i] + "<br>");
