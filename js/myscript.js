b111314ecc039f60783825419231a0ccc132eefc
Cianuro-83 <buzzeosimone@yahoo.com>
2023-02-07 15:28:00 +0100

    TRY: richiesta numero

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,18 @@
 console.log("CIAO CIANURO: oggi CICLO WHILE vs CICLO FOR");
+
+// creo la variabile numero
+let numero;
+// creo array dove pushare i numeri dispari
+let numeriDispari = [];
+// chiedo numero all'utente
+// do {
+numero = parseInt(prompt("Inserisci un numero: "));
+console.log(numero);
+while (isNaN(numero) || numero <= 0 || numeriDispari.lenght >= 10);
+{
+  if (numero % 2 === 1) {
+    numeriDispari.push(numero);
+  }
+  // chiusura ciclo while
+}
+console.log("hai inserito " + numero, numeriDispari);
